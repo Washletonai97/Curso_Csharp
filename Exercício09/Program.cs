222e4bb1c06ece040c6b64d15b7d12df57ffd32a
WASHINGTON LETONAI DA SILVA <washington.lsilva16@senacsp.edu.br>
2025-01-21 19:43:25 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício09/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercício09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.Write("Digite o valor do lado A: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado B: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado C: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
+            {
+                Console.WriteLine("Os valores informados formam um triângulo.");
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados não formam um triângulo.");
+
+            }
+        }
+    }
+}
